7ff3db0809758ae3895d5417820d7a8924625b93
MrKyveee <mrkyveedeveloper@gmail.com>
2026-03-23 18:37:49 -0300

    commit inicial - setup

---
+++ main/java/com/kyvee/gastoapi/GastoapiApplication.java
@@ -0,0 +1,13 @@
+package com.kyvee.gastoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastoapiApplication.class, args);
+	}
+
+}
---
+++ test/java/com/kyvee/gastoapi/GastoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyvee.gastoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
